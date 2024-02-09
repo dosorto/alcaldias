2f8480a2d3a9abfbb72c76ebdfeccfc8d045c722
Alan-Cardona19 <157536049+Alan-Cardona19@users.noreply.github.com>
2024-02-08 23:45:57 -0600

    ho

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Inicio</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <style>
 /* Estilos para el cuerpo de la página */
@@ -23,14 +24,16 @@ body {
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="min-height: 40px;"> <!-- Modificar la altura mínima -->
   <div class="container">
-    <a class="navbar-brand" href="#">Alcaldía Municipal</a>
+    <a class="navbar-brand ml-auto" href="#">Alcaldia Municipal</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Inicio</a>
+          <a class="nav-link" href="#">Inicio <i class="bi bi-house-door-fill"></i></a>
+
+
         </li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -42,6 +45,8 @@ body {
               <a class="dropdown-item" href="#">Documentos De Interes</a>
             </div>
           </li>
+
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Atencion Ciudadano
@@ -53,6 +58,8 @@ body {
             <a class="dropdown-item" href="#">Solvencia Municipal</a>
           </div>
         </li>
+
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Gerencia Catastro
@@ -64,6 +71,7 @@ body {
             <a class="dropdown-item" href="#">Ubicacion De Clave Catastral</a>
           </div>
         </li>
+
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Noticias
@@ -75,22 +83,25 @@ body {
               <a class="dropdown-item" href="#">Ubicacion De Clave Catastral</a>
             </div>
           </li>
+
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Contactos
             </a>
+
             <div class="dropdown-menu" aria-labelledby="contactDropdown">
-              <a class="dropdown-item" href="#"><i class="fab fa-facebook-square"></i> Facebook</a>
-              <a class="dropdown-item" href="#"><i class="fab fa-whatsapp"></i> WhatsApp</a>
-              <a class="dropdown-item" href="#"><i class="fas fa-map-marker-alt"></i> Ubicación</a>
+              <a class="dropdown-item" href="#"> Facebook <i class="bi bi-facebook"></i></a>
+              <a class="dropdown-item" href="#"> Whatsapp <i class="bi bi-whatsapp"></i> </a>
+              <a class="dropdown-item" href="#"> Ubicacion<i class="bi bi-geo-alt"></i></a>
             </div>
+
           </li>
 
           <!-- Modificar el formulario de búsqueda -->
 
           <form class="form-inline my-2 my-lg-0 ml-auto" style="max-width: 250px;"> <!-- Ajustar el tamaño del formulario -->
-            <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search" style="width: 110px;"> <!-- Ajustar el tamaño del campo de búsqueda -->
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+            <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search" style="width: 115px;"> <!-- Ajustar el tamaño del campo de búsqueda -->
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="bi bi-search"></i></button>
           </form>
 
       </ul>
