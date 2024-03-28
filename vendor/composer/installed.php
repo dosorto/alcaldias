fe645a546dc095834c910eefa177f1825cc2f4e3
Nataly0206 <natalyferrufino43@gmail.com>
2024-03-28 15:29:36 -0600

    correcciones

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '02ed5c1108cbb6a6b894c0ef7fa453a4470f7443',
+        'reference' => '09436d591b247d3b7ee819f6bf5b686c2686003c',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -391,7 +391,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '02ed5c1108cbb6a6b894c0ef7fa453a4470f7443',
+            'reference' => '09436d591b247d3b7ee819f6bf5b686c2686003c',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -802,8 +802,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
