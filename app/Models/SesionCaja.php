7065fa1f49df64cea20ec38f564b50311eeb8420
Nataly0206 <natalyferrufino43@gmail.com>
2024-04-06 12:04:52 -0600

    Avances

---
+++ app/Models/SesionCaja.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SesionCaja extends Model
+{
+    use HasFactory;
+    protected $fillable = ['usuario_id', 'monto_inicial', 'created_at', 'closed_at'];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'usuario_id');
+    }
+}
