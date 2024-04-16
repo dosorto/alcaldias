0be043b92f4cd36570c845c30310a0b649d2a4cd
ODAR172023 <157974350+ODAR172023@users.noreply.github.com>
2024-04-15 21:56:03 -0600

    Modulo Caja

--- resources/views/livewire/cobros/cobros.blade.php
+++ resources/views/livewire/cobros/cobros.blade.php
@@ -22,10 +22,18 @@
   </nav>  
 <div class="max-w-6xl mx-auto bg-white p-6 rounded shadow-md">
     
-    <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
-    <p class="font-bold">Se ha iniciado una sesión en caja por el usuario: {{ $usuario->name }}</p>
-    <p class="text-sm"><b>Hora de apertura:</b> {{ $fechainiciocaja }}</p>
+<div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 flex justify-between items-center" role="alert">
+    <div>
+        <p class="font-bold">Se ha iniciado una sesión en caja por el usuario: {{ $usuario->name }}</p>
+        <p class="text-sm"><b>Hora de apertura:</b> {{ $fechainiciocaja }}</p>
     </div>
+    <div class="inline-flex rounded-md shadow-sm">
+        <button wire:click="openModal()" type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium bg-blue-500 text-white border border-transparent rounded-lg hover:bg-blue-700 focus:outline-none focus:border-blue-700 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
+            Cerrar Caja
+        </button>
+    </div>
+</div>
+
 
     <h2 class="text-3xl font-extrabold dark:text-white">Cobros</h2>
     <hr class="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700">
@@ -42,16 +50,7 @@
             <input wire:model.live="search" type="text" id="table-search-users" class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Buscar...">
         </div>
 
-        <div>
-            <div class="inline-flex rounded-md shadow-sm" role="group">
-            <button wire:click="openModal()" type="button" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10  dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white">
-                  <svg class="w-3 h-3 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-                  </svg>
-                  Cerrar Caja
-                </button>
-              </div>
-        </div>
+      
     </div>
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
