533377fce4c54e8f7e09a5031e8da39f4a0713b6
Nataly0206 <natalyferrufino43@gmail.com>
2024-04-20 23:49:33 -0600

    Configuracion General

--- app/Livewire/Configuraciongeneral.php
+++ app/Livewire/Configuraciongeneral.php
@@ -3,11 +3,28 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Configuraciongeneral extends Component
 {
+    use WithFileUploads;
+    public $image;
+    
     public function render()
     {
         return view('livewire.configuraciongeneral');
     }
+
+    public function store()
+{
+    $this->validate([
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Validar que es una imagen válida
+    ]);
+
+    // Procesar la imagen después de la validación
+    $imagePath = $this->image->store('images', 'public'); // Almacenar la imagen en storage/app/public/images
+
+    session()->flash('success', 'Imagen cargada correctamente.');
+    session()->flash('image', $imagePath);
+}
 }
