1810bbcc20264b468096892848724a0f60339552
Alan-Cardona19 <157536049+Alan-Cardona19@users.noreply.github.com>
2024-04-22 12:45:21 -0600

    AL-17

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'f4da187eb38a4e839ae35b30367234a2c09c840c',
+        'reference' => '0b52e428bc9398102edc4bb059f1eaff308233c8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -391,7 +391,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'f4da187eb38a4e839ae35b30367234a2c09c840c',
+            'reference' => '0b52e428bc9398102edc4bb059f1eaff308233c8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -802,8 +802,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
