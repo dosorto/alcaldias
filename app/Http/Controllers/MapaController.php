4289c8328eb588a0cd8deb1bbfd02fa6733b72c8
Dayanni Avila <dayiavilakkv@gmail.com>
2024-07-23 13:47:35 -0600

    correción rama

--- app/Http/Controllers/MapaController.php
+++ app/Http/Controllers/MapaController.php
@@ -5,15 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Georeferenciacion;
 use Illuminate\Http\Request;
 use App\Models\Propiedad;
-
-class MapaController extends Controller
-{
-    public function mapa(string $id)
-    {
-        $coordenadas = Georeferenciacion::where('IdPropiedad', intval($id))->get();
-
-        // convertir coordenadas a una matriz donde solo voy a guardar latitud y longitud
-        // pero no quiero que sea clave valor, solo quiero los valores
 use Maatwebsite\Excel\Concerns\ToArray;
 
 class MapaController extends Controller
