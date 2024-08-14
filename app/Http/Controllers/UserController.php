ea81e43fa81c9f2d3da7fc909f4cd42fb221b743
dao <adolfo_24@yahoo.com>
2024-08-14 14:04:10 -0600

    adding api functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\ApiController;
+use App\Livewire\PagoServicio;
+use App\Models\Contribuyente;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
+use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Log;
 
 class UserController extends ApiController
@@ -33,6 +36,47 @@ class UserController extends ApiController
             $user->token =  $user->createToken("API TOKEN")->plainTextToken;
 
             return $this->successResponse('User successfully logged-in.', $user );
+
+        } catch (\Throwable $th) {
+            return $this->errorResponse('Unauthorized.', ['error'=>'Unauthorized'], 500);
+        }
+    }
+    public function create_user(Request $request)
+    {
+        Log::channel('stderr')->info($request);
+        try {
+            $validateUser = Validator::make($request->all(), 
+            [
+                'name'=>'required',
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+            if($validateUser->fails()){
+                return $this->errorResponse('Falta campos en la petición', ['error'=>'Validadción'], 403);
+            }
+            //resvisar 
+            User::create([
+                'name'=>$request['name'],
+                'email'=>$request['email'],
+                'password'=>$request['password'], ]);
+            // Contribuyente::create([
+
+            // ])
+            
+            return $this->successResponse('usuario creado', $request );
+
+        } catch (\Throwable $th) {
+            return $this->errorResponse('Error en el server.', ['error'=>'error en el server'], 500);
+        }
+    }
+
+    public function get_pagos_by_User(Request $request)
+    {
+        Log::channel('stderr')->info($request);
+        try {
+            //revisar consulta
+            $pagos = PagoServicio::get();
+            return $this->successResponse('Lista de pagos obtenida correctamente', $pagos );
 
         } catch (\Throwable $th) {
             return $this->errorResponse('Unauthorized.', ['error'=>'Unauthorized'], 500);
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 use App\Http\Controllers\UserController;
 Route::post('/login', [UserController::class, 'login']);
+Route::post('/user', [UserController::class, 'create_user']);
+Route::middleware('auth:sanctum')->get('/user/pagos', [UserController::class, 'get_pagos_by_User']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
